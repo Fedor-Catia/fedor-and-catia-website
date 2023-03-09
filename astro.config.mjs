b5f62d80c6c4c62ec49991e5c3925b4c12aa6120
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2023-03-09 20:50:12 +0000

    test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://fedor-catia.github.io',
-  base: '/website',
+  base: '/fedor-and-catia-website/',
 });
