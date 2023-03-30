e04296d439aab4147b8dae64d8924632121cd3ca
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2023-03-30 20:41:56 +0100

    changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			gridTemplateRows: {
+				'7': 'repeat(7, minmax(0, 1fr))',
+				'14': 'repeat(14, minmax(0, 1fr))',
+			  },
 			colors: {
 				primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
 			  },
